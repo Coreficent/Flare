264903aa6968de93fea4442e1dae79e5aba5c485
Coreficent <coreficent@gmail.com>
2021-02-22 14:41:59 -0500

    Clean up.

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <dove/Core.h>
-#include <gsl/gsl>
 
+#include "dove/Core.h"
+#include "gsl/gsl"
 
 int main(int argc, char** argv)
 {
 	Dove::Core core{1600,900};
 	core.run();
-	int arr[10];           // BAD, warning 26494 will be fired
-	int* p = arr;  // BAD, warning 26485 will be fired
+
 	return 0;
 }
