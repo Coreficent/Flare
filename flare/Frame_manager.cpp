740b94432974edee1e2b605f3fd55248b761cc58
Coreficent <coreficent@gmail.com>
2021-03-14 18:49:58 -0400

    Improve the accuracy of frame manager.

--- flare/Frame_manager.cpp
+++ flare/Frame_manager.cpp
@@ -10,32 +10,59 @@ namespace Flare::Frame
 
 	void Frame_manager::calculate_fps()
 	{
-		this->current_ticks = SDL_GetTicks();
-		auto current_cost = this->current_ticks - this->previous_ticks;
-		this->previous_ticks = this->current_ticks;
+		this->a = std::chrono::system_clock::now();
+		std::chrono::duration<double, std::milli> work_time = this->a - this->b;
 
-		this->samples.at(this->current_frame++ % this->samples.size()) = current_cost;
+		double second{ 1000.0 };
+		double budget{ second / 60.0 };
 
-		Uint32 average_cost{ 0 };
 
-		for (auto& i : this->samples)
+		if (work_time.count() < budget)
 		{
-			average_cost += i;
+			std::chrono::duration<double, std::milli> delta_ms(budget - work_time.count());
+			auto delta_ms_duration = std::chrono::duration_cast<std::chrono::milliseconds>(delta_ms);
+			std::this_thread::sleep_for(std::chrono::milliseconds(delta_ms_duration.count()));
 		}
 
-		const auto fps = 1000.0f / ((average_cost + 0.0001f) / this->samples.size());
-		const int remaining_budget = this->budget - current_cost;
+		this->b = std::chrono::system_clock::now();
+		std::chrono::duration<double, std::milli> sleep_time = this->b - this->a;
 
-		if (this->budget > current_cost)
-		{
-			SDL_Delay(remaining_budget);
-		}
+		// Your code here
 
-		if (!(this->current_frame % 60))
-		{
-			cout << "fps: " << fps << " budget " << remaining_budget << endl;
-		}
+		// printf("Time: %f \n", (work_time + sleep_time).count());
 
-		this->frames_per_second = fps;
+		this->frames_per_second = second / (work_time + sleep_time).count();
+
+		
+
+
+
+		//this->current_ticks = SDL_GetTicks();
+		//auto current_cost = this->current_ticks - this->previous_ticks;
+		//this->previous_ticks = this->current_ticks;
+
+		//this->samples.at(this->current_frame++ % this->samples.size()) = current_cost;
+
+		//Uint32 average_cost{ 0 };
+
+		//for (auto& i : this->samples)
+		//{
+		//	average_cost += i;
+		//}
+
+		//const auto fps = 1000.0f / ((average_cost + 0.0001f) / this->samples.size());
+		//const int remaining_budget = this->budget - current_cost;
+
+		//if (this->budget > current_cost)
+		//{
+		//	SDL_Delay(remaining_budget);
+		//}
+
+		//if (!(this->current_frame % 60))
+		//{
+		//	cout << "fps: " << fps << " budget " << remaining_budget << endl;
+		//}
+
+		//this->frames_per_second = fps;
 	}
 }
--- flare/Frame_manager.h
+++ flare/Frame_manager.h
@@ -2,9 +2,13 @@
 
 #include <sdl/SDL_stdinc.h>
 #include <array>
+#include <chrono>
+#include <thread>
 
 namespace Flare::Frame
 {
+	using namespace std::chrono;
+
 	class Frame_manager
 	{
 
@@ -19,6 +23,9 @@ namespace Flare::Frame
 		Uint32 current_ticks{ 0 };
 		Uint32 current_frame{ 0 };
 		Uint32 previous_ticks{ 0 };
+
+		system_clock::time_point a = system_clock::now();
+		system_clock::time_point b = system_clock::now();
 
 		unsigned int budget{ 16 };
 	};
