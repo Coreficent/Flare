6078b8a1af8ae113be7071bf2396bea95ba63d35
Coreficent <coreficent@gmail.com>
2021-03-14 00:46:13 -0500

    Clean up container.

--- flare/Container.cpp
+++ flare/Container.cpp
@@ -4,30 +4,13 @@ namespace Flare::Display
 {
 	Container::Container() noexcept {}
 
-	void Container::add_child(std::shared_ptr<Flare::Display::Sprite> child)
+	void Container::add_child(Sprite child)
 	{
 		this->children.push_back(child);
 	}
 
-	void Container::remove_child(std::shared_ptr<Flare::Display::Sprite> child) noexcept
-	{
-		unsigned int i{ 0 };
-		for (auto& c : this->children)
-		{
-			if (c == child)
-			{
-				break;
-			}
-			++i;
-		}
-		this->children.erase(this->children.begin() + i);
-	}
-
 	void Container::render() const
 	{
-		for (auto& c : this->children)
-		{
-			c->render();
-		}
+		
 	}
 }
--- flare/Container.h
+++ flare/Container.h
@@ -6,16 +6,18 @@
 
 namespace Flare::Display
 {
+	using namespace Flare::Display;
+
 	class Container
 	{
 	public:
 		Container() noexcept;
 
-		void add_child(std::shared_ptr<Flare::Display::Sprite> child);
-		void remove_child(std::shared_ptr<Flare::Display::Sprite> child) noexcept;
+		void add_child(Sprite child);
+		
 		void render() const;
 
 	private:
-		std::vector<std::shared_ptr<Flare::Display::Sprite>> children;
+		std::vector<Sprite> children;
 	};
 }
