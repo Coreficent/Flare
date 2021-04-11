8333e6fd6f6ec110450fafae9579dd6ba1098a84
Stephen Xie <coreficent@gmail.com>
2021-04-11 03:29:36 -0400

    Add ground and improve error handling.

--- game/Game_core.cpp
+++ game/Game_core.cpp
@@ -10,11 +10,25 @@ namespace Game {
 
 	void Game_core::initialize() noexcept
 	{
-		shared_ptr<Sprite> sky{ new Sprite{"texture/Sky.png" } };
-		sky->width = 1600;
-		sky->height = 900;
+		try
+		{
+			shared_ptr<Sprite> sky{ make_shared<Sprite>("texture/Sky.png") };
 
-		this->get_stage().add_child(sky);
+			sky->width = 1600;
+			sky->height = 900;
+			this->get_stage().add_child(sky);
+
+			shared_ptr<Sprite> ground{ make_shared<Sprite>("texture/Ground.png") };
+			ground->width = 2000;
+			ground->height = 200;
+			ground->y = 450;
+			this->get_stage().add_child(ground);
+		}
+		catch (...)
+		{
+			printf("unable to initialize game core");
+		}
+
 	}
 
 	void Game_core::enter_frame() noexcept
