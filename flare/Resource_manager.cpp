ca1a93e3d1fe261e769c1ea7adca45c135f697f1
Stephen Xie <coreficent@gmail.com>
2021-04-12 14:52:24 -0400

    Clean up resource manager.

--- flare/Resource_manager.cpp
+++ flare/Resource_manager.cpp
@@ -1,3 +1,4 @@
+#include "ImageLoader.h"
 #include "Resource_manager.h"
 
 namespace Flare
@@ -6,7 +7,14 @@ namespace Flare
 
 	GLTexture Resource_manager::get_texture(string file_path)
 	{
-		return texture_cache.get_texture(file_path);
+		auto it = Resource_manager::texture_map.find(file_path);
+		if (it == Resource_manager::texture_map.end())
+		{
+			auto texture = ImageLoader::loadPNG(file_path);
+			Resource_manager::texture_map.insert({ file_path,texture });
+			return texture;
+		}
+		return it->second;
 	}
 
 	Mix_Chunk* Resource_manager::get_audio(const std::string& file_path)
--- flare/Resource_manager.h
+++ flare/Resource_manager.h
@@ -1,19 +1,24 @@
 #pragma once
 
-#include "Texture_cache.h"
+#include "GlTexture.h"
+
+#include <map>
 #include <sdl/SDL_mixer.h>
+#include <string>
 
 namespace Flare
 {
+	using namespace std;
+
 	class Resource_manager
 	{
 
 	public:
-		static GLTexture get_texture(std::string file_path);
-		static Mix_Chunk* Resource_manager::get_audio(const std::string& file_path);
+		static GLTexture get_texture(string file_path);
+		static Mix_Chunk* Resource_manager::get_audio(const string& file_path);
 
 	private:
-		inline static Texture_cache texture_cache{};
+		inline static map<string, GLTexture> texture_map{};
 		inline static map<string, Mix_Chunk*> effect_map{};
 	};
 
